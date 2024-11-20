1233422b1f8c6faad01d61f13411dd3907d260d0
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2024-11-20 16:09:49 -0700

    Modify Seat and Ticket relationships. Update methods in ticket class.

--- Maven/ACME/src/main/java/ENSF614Group1/ACME/Model/Seat.java
+++ Maven/ACME/src/main/java/ENSF614Group1/ACME/Model/Seat.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
@@ -34,21 +35,31 @@ public class Seat {
 	@JsonView(Views.TheaterDetail.class)
     private SeatStatus status;
 	
+	@JsonView(Views.TheaterDetail.class)
+	private double cost;
+	
 	@ManyToOne
 	@JoinColumn(name = "showtime_id", nullable = false)
 	private Showtime showtime;
+	
+	@OneToOne(mappedBy = "seat")
+	@JsonIgnore
+	private Ticket ticket;
+	
 	
 	// Getters
 	public Long getId() {return id;}
 	public int getSeatRow() {return seatRow;}
 	public int getSeatNumber() {return seatNumber;}
 	public SeatStatus getStatus() {return status;}
+	public double getCost() {return cost;}
 	public Showtime getShowtime() {return showtime;}
 	
 	// Setters
 	public void setSeatRow(int seatRow) {this.seatRow = seatRow;}
 	public void setSeatNumber(int seatNumber) {this.seatNumber = seatNumber;}
 	public void setStatus(SeatStatus status) {this.status = status;}
+	public void setCost(double cost) {this.cost = cost;}
 	public void setShowtime(Showtime showtime) {this.showtime = showtime;}
 		
 	// Constructors
@@ -57,12 +68,13 @@ public class Seat {
 		this.seatRow = seatRow;
 		this.seatNumber = seatNumber;
 		this.status = SeatStatus.AVAILABLE;
+		this.cost = 10.99;
 		this.showtime = showtime;
 	}
 	
 	public enum SeatStatus {
 	    AVAILABLE,
-	    RESERVED,
-	    OCCUPIED
+	    INCART,
+	    BOOKED
 	}
 }
--- Maven/ACME/src/main/java/ENSF614Group1/ACME/Model/Ticket.java
+++ Maven/ACME/src/main/java/ENSF614Group1/ACME/Model/Ticket.java
@@ -8,41 +8,43 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
-	private User user;
-    
-    @ManyToOne
-    @JoinColumn(name = "theater_id", nullable = false)
-    private Theater theater;
-    
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "seat_id", nullable = false)
     private Seat seat;
     
     @ManyToOne
-    @JoinColumn(name = "showtime_id", nullable = false)
-    private Showtime showtime;
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+    
+    private TicketStatus status;
     
     public Long getID() {return id;}
     public User getUser() {return user;}
-    public Theater getTheater() {return theater;}
     public Seat getSeat() {return seat;}
-    public Showtime getShowtime() {return showtime;}
+    public Cart getCart() {return cart;}
+    public TicketStatus getStatus() {return status;} 
     
     public void setUser(User user) {this.user = user;}
-    public void setTheater(Theater theater) {this.theater = theater;}
     public void setSeat(Seat seat) {this.seat = seat;}
-    public void setShowtime(Showtime showtime) {this.showtime = showtime;}
+    public void setCart(Cart cart) {this.cart = cart;}
+    public void setStatus(TicketStatus status) {this.status = status;}
     
     public Ticket() {
     }
     
-    public Ticket(User user, Theater theater, Seat seat, Showtime showtime) {
-    	this.user = user;
-    	this.theater = theater;
+    public Ticket(Cart cart, Seat seat) {
+    	this.cart = cart;
     	this.seat = seat;
-    	this.showtime = showtime;
+    	this.status = TicketStatus.INCART;
     }
+    
+    public enum TicketStatus {
+	    INCART,
+	    PURCHASED
+	}
     
 }
