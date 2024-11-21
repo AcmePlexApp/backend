e9ca160286b27ca3f926bb6fe49b927f7f0adbed
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2024-11-21 16:04:49 -0700

    Create service class methods for adding/ removing tickets from cart, purchasing tickets in cart, and cancelling purchased tickets.

--- Maven/ACME/src/main/java/ENSF614Group1/ACME/Controller/UserController.java
+++ Maven/ACME/src/main/java/ENSF614Group1/ACME/Controller/UserController.java
@@ -1,8 +1,5 @@
 package ENSF614Group1.ACME.Controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +7,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ENSF614Group1.ACME.Model.CreditCard;
-import ENSF614Group1.ACME.Model.Movie;
 import ENSF614Group1.ACME.Model.RegisteredUser;
-import ENSF614Group1.ACME.Model.Theater;
 import ENSF614Group1.ACME.Model.User;
 import ENSF614Group1.ACME.Security.JWTUtil;
 import ENSF614Group1.ACME.Service.UserService;
-import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/user")
